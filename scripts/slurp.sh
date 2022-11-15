77d24a35b584a9c60d389191e6bb815252738df0
Alexander Eldeib <aleldeib@microsoft.com>
2022-11-15 23:41:10 +0000

    fix: add github release creation

--- scripts/slurp.sh
+++ scripts/slurp.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-set -o errexit
 
-# strip whitespace from filename "$1" and pipe back to same file.
-git stripspace < "$1" | sponge "$1"
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	against=$(git hash-object -t tree /dev/null)
+fi
+
+# If there are whitespace errors, print the offending file names and fail.
+# Find files with trailing whitespace
+for FILE in `exec git diff-index --check --cached $against -- | sed '/^[+-]/d' | sed -r 's/:[0-9]+:.*//' | uniq` ; do
+    # Fix them!
+    git stripspace < "$FILE" | sponge "$FILE"
+	git add "$FILE"
+done
