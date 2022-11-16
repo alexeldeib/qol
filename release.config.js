9b21bde7be673e837bd26b195f6c56b2666c33be
Alexander Eldeib <aleldeib@microsoft.com>
2022-11-16 21:13:01 +0000

    chore: convert releaserc to js module

---
+++ release.config.js
@@ -0,0 +1,52 @@
+const dateformat = import('dateformat');
+
+module.exports = {
+    branches: [
+        "main"
+    ],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    { type: "feat", release: "minor" },
+                    { type: "fix", release: "patch" },
+                    { type: "build", release: "patch" },
+                    { type: "chore", release: "patch" },
+                    { type: "ci", release: "patch" },
+                    { type: "perf", release: "patch" },
+                    { type: "refactor", release: "patch" }
+                ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                presetConfig: {
+                    types: [
+                        { type: "feat", section: "Features" },
+                        { type: "feature", section: "Features" },
+                        { type: "fix", section: "Bug Fixes" },
+                        { type: "perf", section: "Performance Improvements" },
+                        { type: "revert", section: "Reverts" },
+                        { type: "docs", section: "Documentation" },
+                        { type: "style", section: "Miscellaneous" },
+                        { type: "chore", section: "Miscellaneous" },
+                        { type: "refactor", section: "Miscellaneous" },
+                        { type: "test", section: "Tests" },
+                        { type: "build", section: "Build/CI/CD" },
+                        { type: "ci", section: "Build/CI/CD" }
+                    ]
+                },
+            }
+        ],
+        "@semantic-release/changelog",
+        [
+            "@semantic-release/npm",
+            {
+                "npmPublish": false,
+            }
+        ],
+    ],
+    preset: "conventionalcommits"
+}
