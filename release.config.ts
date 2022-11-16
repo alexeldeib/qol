0b72350125cc3ef6d9b7d12b571d300353296faa
Alexander Eldeib <aleldeib@microsoft.com>
2022-11-16 17:49:15 +0000

    chore: convert semantic release config to js

---
+++ release.config.ts
@@ -0,0 +1,54 @@
+const dateFormat = require('dateformat')
+
+module.exports = {
+    branches: [
+        "main"
+    ],
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                releaseRules: [
+                    {type: "feat",release: "minor"},
+                    {type: "fix",release: "patch"},
+                    {type: "build",release: "patch"},
+                    {type: "chore",release: "patch"},
+                    {type: "ci",release: "patch"},
+                    {type: "perf",release: "patch"},
+                    {type: "refactor",release: "patch"}
+                ]
+            }
+        ],
+        [
+            "@semantic-release/release-notes-generator",
+            {
+                presetConfig: {
+                    types: [
+                        {type: "feat",section: "Features"},
+                        {type: "feature",section: "Features"},
+                        {type: "fix",section: "Bug Fixes"},
+                        {type: "perf",section: "Performance Improvements"},
+                        {type: "revert",section: "Reverts"},
+                        {type: "docs",section: "Documentation"},
+                        {type: "style",section: "Miscellaneous"},
+                        {type: "chore",section: "Miscellaneous"},
+                        {type: "refactor",section: "Miscellaneous"},
+                        {type: "test",section: "Tests"},
+                        {type: "build",section: "Build/CI/CD"},
+                        {type: "ci",section: "Build/CI/CD"}
+                    ]
+                },
+                writerOpts: {
+                    date: function (date) {
+                        return dateFormat(date, 'isoUtcDateTime')
+                    },
+                    // committerDate: function (date) {
+                    //     return dateFormat(date, 'isoUtcDateTime')
+                    // },
+                }
+            }
+        ],
+        "@semantic-release/changelog"
+    ],
+    preset: "conventionalcommits"
+}
