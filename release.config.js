335f3fb656b5da91e9a0f74d2b748b6593883ead
Alexander Eldeib <aleldeib@microsoft.com>
2022-11-16 23:16:25 +0000

    chore: remove npm, add github plugin

--- release.config.js
+++ release.config.js
@@ -38,12 +38,7 @@ module.exports = {
                 },
             }
         ],
-        [
-            "@semantic-release/npm",
-            {
-                "npmPublish": false,
-            }
-        ],
+        "@semantic-release/github"
     ],
     preset: "conventionalcommits"
 }
